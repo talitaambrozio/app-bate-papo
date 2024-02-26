8a57e7dc37689d3304627441f8b8a4be887e3560
talitaambrozio <talitalmeidas@gmail.com>
2024-02-26 15:49:27 -0300

    feat: teste 2 semantic-release

--- release.config.js
+++ release.config.js
@@ -5,7 +5,8 @@ const config= {
         '@semantic-release/release-notes-generator',
         ["@semantic-release/git", {
             "assets": ["dist/*.js", "dist/*.js.map"],
-            "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+           // "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+           "message": "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}"
         }],
         '@semantic-release/github'
     ]
